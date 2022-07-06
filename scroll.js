859e2cdcadb4ede34a2f0a620da5a78c010d8f46
Shin-WhetherWeather <shin.whetherweather@gmail.com>
2022-07-06 15:41:13 +1000

    fixed scrolling?

--- scroll.js
+++ scroll.js
@@ -1,6 +1,7 @@
 function topFunction() {
  
     $('html, body').animate({scrollTop:0}, 'slow');
-    console.log("click");
+    $(window).animate({scrollTop:0}, 'slow');
+    console.log("scroll top");
   }
   
